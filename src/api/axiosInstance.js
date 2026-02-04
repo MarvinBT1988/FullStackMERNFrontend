6f23f1734c59a1e8918524b99db2e4733037ef19
Marvin Barrera <59856529+MarvinBT1988@users.noreply.github.com>
2026-02-04 21:44:14 +0000

    token estatico

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,10 +1,12 @@
 import axios from 'axios';
 const apiUrl = import.meta.env.VITE_APIMANT_URL;
+const token = 'tu_token_aqui';
 const axiosInstance = axios.create({ 
     baseURL: apiUrl,
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
     },
 });
 export default axiosInstance;
